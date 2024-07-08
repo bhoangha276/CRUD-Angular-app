56a6a9ed02ec3b751fbd256fd413384f709d80ce
hahoang <bhoangha276@gmail.com>
2024-07-08 23:36:05 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { IndexComponent } from './product/index/index.component';
+import { ViewComponent } from './product/view/view.component';
+
+export const routes: Routes = [
+  { path: 'product', redirectTo: 'product/index', pathMatch: 'full' },
+  { path: 'product/index', component: IndexComponent },
+  { path: 'product/:productId/view', component: ViewComponent },
+];
